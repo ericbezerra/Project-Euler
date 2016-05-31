1a4f5ede960749603f1dff1a17d6be72a4e657ca
Eric Bezerra <ericshussan@hotmail.com>
2016-05-31 10:01:47 -0300

    Fibonacci clear

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,18 +1,24 @@
-public class Fibonacci {
-    public static void main(String[] args){
-	int sum = 0;
-	for(int i = 2; i < 4000000; i++){
-	    if(fibonnaci(i)>=4000000)break;
-	    sum += fibonnaci(i);
-	    System.out.println(fibonnaci(i));
-	}
-	System.out.println("Sum of fibonnaci is = " + sum);
-    }
-    public static int fibonnaci(int n){
-	if(n < 2){
-	    return n;
-	}else{
-	    return fibonnaci(n - 1) + fibonnaci(n - 2);
-	}
-    }
+public class Fibonacci{
+  public static void main(String[]args){
+    int limit = 4000000;
+    fibo(limit);
+  }
+  public static void fibo(int limit){
+    int x = 0;
+    int y = 1;
+    int z = 0;
+    int sum = 0;
+
+    System.out.println(x);
+    System.out.println(y);
+
+    do{
+      z = x + y;
+      x = y;
+      y = z;
+      if(y % 2 == 0) sum += y;
+      System.out.println(z);
+    }while(y <= limit);
+    System.out.println("Fibonacci sum = " + sum);
+  }
 }
